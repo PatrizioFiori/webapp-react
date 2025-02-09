6d06fb888cc05bafd0cac706075f3d8fb2961ba0
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-09 18:50:02 +0100

    Mileston 3 completa (stampate a schermo le cards dei movies)

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,21 @@
+import { useNavigate, useParams } from "react-router-dom"
 
 const Card = ({ item }) => {
+
     return (
-        <div>Card</div>
+
+        < div className="card-custom-info border text-center px-3 py-2" >
+            <img className="py-2" src={item.image} alt="img-movie" style={{
+                width: "100%",
+                height: "auto",
+                border: "1px solid black",
+            }} />
+            <div>
+                <h5>{item.title}</h5>
+                <p>{item.abstract}</p>
+                <a className="btn border text-bg-primary" href={`/movie/${item.id}`} >Details</a>
+            </div>
+        </div>
     )
 }
 
--- pages/MoviesList.jsx
+++ pages/MoviesList.jsx
@@ -16,14 +16,15 @@ const MoviesList = () => {
             <div className="container d-flex justify-content-center my-4">
                 <div className="row mb-4">
                     {listaFilm.map((item) => (
-                        <div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-4" key={item.id}>
+                        <div div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-4" key={item.id} >
                             <Card item={item} />
+
                         </div>
                     ))}
                 </div>
             </div>
 
-        </div>
+        </div >
     )
 }
 
